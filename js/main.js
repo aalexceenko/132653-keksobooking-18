4c10034a61b2a7ad961a13e1da3a387951e3d04e
aalexceenko <xnyxnymrik@bk.ru>
2019-09-23 15:01:25 +0300

    create array for pins

--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@ var TYPE_HOUSE = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS_HOUSE = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PHOTOS_HOUSE = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
 var X_MAX = 900;
 var X_MIN = 300;
 var Y_MAX = 630;
 var Y_MIN = 130;
+
+var DESCRIPTIONS = ['Сдам жилье 1'];
+var TITLES = ['Заголовок'];
 
 
 var getRandomInteger = function (min, max) {
@@ -20,48 +27,69 @@ var getRandomElement = function (arr) {
   return arr[window.getRandomInteger(0, arr.length - 1)];
 };
 
-var featureArr = [];
-var FEATURES_MIN = 0;
-var FEATURES_MAX = 6;
-var FEATURES_LENGTH = getRandomInteger(FEATURES_MIN, FEATURES_MAX);
+var getRandomFeatures = function (features) {
+  var restFeatures = features.slice();
+  var countFeatures = getRandomInteger(1, restFeatures.length);
+  var result = [];
+  for (var i = 0; i < countFeatures; ++i) {
+    var selectedElement = restFeatures.splice(getRandomInteger(0, restFeatures.length - 1), 1);
+    result.push(selectedElement);
+  }
 
-for (var i = 0; i < FEATURES_LENGTH; i++) {
-   featureArr[i] = FEATURES[i];
-}
+  return result;
+};
 
-
-
-var generateCards = function () {
+var generateCards = function (n) {
+  var location = {
+    x: getRandomInteger(X_MIN, X_MAX),
+    y: getRandomInteger(Y_MIN, Y_MAX),
+  };
   return {
-        author: 'img/avatars/user0' + [i + 1] + '.png',
+    author: {
+      avatar: 'img/avatars/user0' + n + '.png',
+    },
 
-        offer: {
-          title: '',
-          address: location.x + ',' + location.y,
-          price: Math.ceil(Math.random() * 1000),
-          type: getRandomElement(TYPE_HOUSE),
-          rooms: Math.ceil(Math.random() * 5),
-          guests: Math.ceil(Math.random() * 5),
-          checkin: getRandomElement(CHECKIN),
-          checkout: getRandomElement(CHECKOUT),
-          features: featureArr,
-          description: '',
-          photos: PHOTOS_HOUSE,
-        },
+    offer: {
+      title: getRandomElement(TITLES),
+      address: location.x + ',' + location.y,
+      price: getRandomInteger(1, 1000),
+      type: getRandomElement(TYPE_HOUSE),
+      rooms: getRandomInteger(1, 5),
+      guests: getRandomInteger(1, 5),
+      checkin: getRandomElement(CHECKIN),
+      checkout: getRandomElement(CHECKOUT),
+      features: getRandomFeatures(FEATURES),
+      description: getRandomElement(DESCRIPTIONS),
+      photos: PHOTOS_HOUSE,
+    },
 
-        location: {
-          x: getRandomInteger(X_MIN, X_MAX),
-          y: getRandomInteger(Y_MIN, Y_MAX)
-        }}
-      };
+    location: location,
+  };
+};
 
-      for (var i = 0; i < COUNT_CARDS; i++) {
-        var card = generateCards();
 
-    }
 document.querySelector('.map').classList.remove('map--faded');
-var PIN_WIDTH = 50;
-var PIN_HEIGHT = 70;
-var pointTemplate = document.querySelector('.map__pin');
+
+
+var mapPinsElement = document.querySelector('.map__pins');
+
+for (var i = 0; i < COUNT_CARDS; i++) {
+  var card = generateCards(i + 1);
+
+  var pinElementTemplate = document.querySelector('#pin').content;
+
+  var pinElement = pinElementTemplate.cloneNode(true);
+  var PIN_WIDTH = 50;
+  var PIN_HEIGHT = 70;
+
+  pinElement.querySelector('.map__pin').style = 'left: ' + (card.location.x - PIN_WIDTH / 2) + 'px; top: ' + (card.location.y - PIN_HEIGHT) + 'px';
+
+  var pinImageElement = pinElement.querySelector('.map__pin img');
+  pinImageElement.src = card.author.avatar;
+  pinImageElement.alt = card.offer.title;
+
+
+  mapPinsElement.appendChild(pinElement);
+}
 
 
